163d27991ab192400e326c2909a090dc636c21a9
101JamesLi <126320976+101JamesLi@users.noreply.github.com>
2023-02-25 17:19:50 +0800

    Add files via upload

---
+++ experiment.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{	
+	char c;
+	char d;
+	scanf("%c %c", &c, &d);
+	printf("%d %c ",c , c);
+	printf("%d %c",d , d);
+		
+	return 0;
+}
